5ca821f311a3a105f616a6a045fdcc40254600da
trunghieu <trunghieu9102002@gmail.com>
2024-05-16 00:15:17 +0700

    remove polygon

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from src.service.crawler.crawl_scan import ScanCrawler
 from src.database.mongodb.mongodb import MongoDB
 
 db = MongoDB()
-cursor = db.get_social_users_by_filter(filter_={'chainId': '0x1'}, projection=['address', 'balanceUSD'])
+cursor = db.get_social_users_by_filter(filter_={'chainId': '0x89'}, projection=['0x89'])
+
+wallets = []
+count = 0
+for doc in cursor:
+    count += 1
+    txs = doc.get('0x89')
+    print(f"Execute {doc.get('_id')} {count}")
+    if len(txs) < 20:
+        print(f"Remove {doc.get('_id')} with len {len(txs)}")
+        db.social_users_col.delete_one(filter={'_id': doc.get('_id')})
 
 # anan = TransactionsAnalysis(api_key='G1JCHT1P7ZWFNB8T2EBZU916VZ35H8R69M')
 # anan.get_balance_by_api(cursor)
 
-scan = ScanCrawler()
-scan.crawl_balance_usd(cursor=cursor)
+# scan = ScanCrawler()
+# scan.crawl_balance_usd(cursor=cursor)
