1e45b4f1779b8974b4abe7de09d26b4105c6909e
trunghieu <trunghieu9102002@gmail.com>
2024-05-16 01:04:14 +0700

    crawl balance polygon

--- src/service/crawler/crawl_scan.py
+++ src/service/crawler/crawl_scan.py
@@ -65,7 +65,5 @@ class ScanCrawler:
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         chrome_options.add_argument(f'user-agent={user_agent}')
         chrome_options.add_argument("window-size=1920,1080")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
         return driver
